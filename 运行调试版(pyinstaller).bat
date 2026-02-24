496890262a275789516a83f10b6d788d9821a1bd
nj-yzf <nj_yzf@163.com>
2026-02-24 10:11:36 +0800

    修复 .gitignore

--- 运行调试版(pyinstaller).bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-set WOA_DEBUG=1
-start "" "WOA_Debug.exe"
-endlocal
--- 运行调试版(点击后约10s没有任何窗口，是正常现象，nuitka).bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-set WOA_DEBUG=1
-start "" "WOA_AutoBot_v1.2.4.2.exe"
-endlocal
